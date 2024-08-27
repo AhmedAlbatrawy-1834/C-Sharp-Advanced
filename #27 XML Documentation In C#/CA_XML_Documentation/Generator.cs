3635f711696672d8e22d38819272867ed5cf4e3e
Ahmed Albatrawy <albatrawyahmed@gmail.com>
2024-08-27 09:51:11 +0300

    Upload Files commit

---
+++ #27 XML Documentation In C#/CA_XML_Documentation/Generator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace CA_XML_Documentation
+{
+    /// <include file="GeneratorXML.xml" path="docs/members[@name='Generator']/Generator/*" />
+    public class Generator
+    {
+        /// <include file="GeneratorXML.xml" path="docs/members[@name='Generator']/LastSequence/*"/>
+        public static int LastSequence { get; private set; } = 1;
+
+        /// <include file="GeneratorXML.xml" path="docs/members[@name='Generator']/GenerateId/*"/>
+        public static string GenerateId(string FName, string LName, DateTime? HTime)
+        {
+            if (string.IsNullOrEmpty(FName))
+                throw new InvalidOperationException($"{nameof(FName)} can not be null or Empty");
+
+            if (string.IsNullOrEmpty(LName))
+                throw new InvalidOperationException($"{nameof(LName)} can not be null or Empty");
+
+            if (HTime == null)
+            {
+                HTime = DateTime.Now;
+            }
+            else
+            {
+                if (HTime.Value.Date < DateTime.Now.Date)
+                {
+                    throw new InvalidOperationException("you can not hire Employee in past");
+                }
+            }
+            var yy = HTime.Value.ToString("yy");
+            var mm = HTime.Value.ToString("MM");
+            var dd = HTime.Value.ToString("dd");
+            var code = $"{FName[0]}{LName[0]}-{yy}-{mm}-{dd}-{(LastSequence++).ToString().PadLeft(2, '0')}";
+
+            return code;
+        }
+
+        public static string GeneratePassword(int Len)
+        {
+            const string ValidScope = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string password = string.Empty;
+            while (0 < Len--)
+            {
+                password += (ValidScope[Random.Shared.Next(ValidScope.Length)]);
+            }
+
+            return password;
+        }
+    }
+}
---
+++ #27 XML Documentation In C#/CA_XML_Documentation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.Emit;
+namespace CA_XML_Documentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+
+                Console.Write("First Name:");
+                var fname = Console.ReadLine();
+                Console.Write("Last Name:");
+                var lname = Console.ReadLine();
+
+                Console.Write("Hire Date:");
+                DateTime? HireDate = null;
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime HDate))
+                {
+                    HireDate = HDate;
+                }
+                var Id = Generator.GenerateId(fname, lname, HireDate);
+                var Password = Generator.GeneratePassword(8);
+
+                Console.WriteLine($"{{\n Id:{Id},\n FName:{fname},\n LName:{lname},\n HireDate:{HireDate.Value.ToShortDateString()},\n Password:{Password}\n}}");
+            } while (true);
+
+        }
+    }
+}
